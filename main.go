88372cce1cb30d8da8a58f2b20cdf532c12b0b07
dani <daniellesubject@gmail.com>
2022-02-16 15:52:38 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
+/* Questions to think about:
+1. How do we run the code in our project?
+2. What does 'package main' mean?
+3. What does 'import "fmt"' mean?
+4. How is the main.go file organized?
+*/
+
+/* Go Run vs Go Build
+Go Run --> compiles and executes one or two files
+Go Build --> Compiles a bunch of go source code files
+Go fmt --> formats all the code in each file in the current directory
+*/
